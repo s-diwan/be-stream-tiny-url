a01412e71221ae186bba4ac1b523d3fa705906bd
sahil-diwan <isahildiwan101@gmail.com>
2020-09-13 02:51:52 +0530

    Commit

--- src/main/java/com/tinyurl/tinyserver/config/SecurityConfiguration.java
+++ src/main/java/com/tinyurl/tinyserver/config/SecurityConfiguration.java
@@ -38,13 +38,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	};
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// TODO Auto-generated method stub
 		auth.userDetailsService(userDetailsService);
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
 		http.csrf().disable()
 		.authorizeRequests().antMatchers(byPassList).permitAll().
 		anyRequest().authenticated()
